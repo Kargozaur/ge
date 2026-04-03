2759d080a23e7cc377fc17b416aa3ff1ec38e2b2
Illia <lpodildaviduk@gmail.com>
2026-04-03 14:10:27 +0200

    jwt

---
+++ cmd/auth/auth_test.go
@@ -0,0 +1,58 @@
+package auth
+
+import (
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func TestTokenManager(t *testing.T) {
+	mgr := &TokenKey{SecretKey: []byte("super-secret-key")}
+	userID := uuid.New()
+
+	t.Run("CreateAndVerify_Success", func(t *testing.T) {
+		tokenStr, err := mgr.CreateAccessToken(userID)
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+
+		err = mgr.VerifyToken(tokenStr)
+		if err != nil {
+			t.Fatalf("Expected token to be valid, got error: %v", err)
+		}
+	})
+
+	t.Run("Verify_InvalidToken", func(t *testing.T) {
+		err := mgr.VerifyToken("not.a.valid.token")
+		if err == nil {
+			t.Error("Expected error for invalid token string, but got nil")
+		}
+	})
+
+	t.Run("Verify_WrongKey", func(t *testing.T) {
+		tokenStr, _ := mgr.CreateAccessToken(userID)
+
+		wrongMgr := &TokenKey{SecretKey: []byte("another-key")}
+		err := wrongMgr.VerifyToken(tokenStr)
+
+		if err == nil {
+			t.Error("Error expected, instead got nil(WrongKey)")
+		}
+	})
+
+	t.Run("Verify_ExpiredToken", func(t *testing.T) {
+		expiredClaims := jwt.MapClaims{
+			"sub": userID.String(),
+			"exp": time.Now().Add(-time.Minute).Unix(),
+		}
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, expiredClaims)
+		tokenStr, _ := token.SignedString(mgr.SecretKey)
+
+		err := mgr.VerifyToken(tokenStr)
+		if err == nil {
+			t.Error("Error expected, instead got nil(ExpiredToken)")
+		}
+	})
+}
